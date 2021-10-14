59b666c37dd2bb913260a1f146696d63c68b3922
Alexis Alvarez <alexisao@hotmail.com>
2021-10-13 23:32:32 -0500

    Componente que representa el Header de la aplicacion

---
+++ src/components/MenuCabecera.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import {NavLink} from 'react-router-dom';
+import logoCabecera from '../assets/dog_bone.png';
+import '../css/hamburger.css';
+import HamburgerButton from './HamburgerButton';
+import './MenuCabecera.css';
+
+const MenuCabecera = () => (
+  <>    
+    <header className = "header">
+    <HamburgerButton panel=".panel" hamburgerButton=".hamburger-button" panelOption=".panel-options"/>
+      <img src={logoCabecera} alt="hueso de perro"/>
+      <nav className="option-group">
+        <NavLink exact className="option" activeClassName="header-link-active" to="/login">Iniciar sesión</NavLink>
+        <NavLink exact className="option" activeClassName="header-link-active" to="/">Inicio</NavLink>
+      </nav>
+    </header>
+  </>
+);
+
+export default MenuCabecera;

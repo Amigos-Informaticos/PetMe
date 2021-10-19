728cfd3fa62486868d2232531af3915264f68a99
Alexis Alvarez <alexisao@hotmail.com>
2021-10-19 18:56:14 -0500

    Cambios en el login
    *Se agregaron las PropTypes al Login
    * Se agregó especificidad para que los estilos solo afecten al FormularioLogin

--- src/components/FormularioLogin.jsx
+++ src/components/FormularioLogin.jsx
@@ -4,50 +4,52 @@ import { Loader } from './Loader';
 import { Message } from './Message';
 import './FormularioLogin.css';
 import { Link } from 'react-router-dom';
+import { codeMessage } from '../helpers/CodeMessage';
+import Validator from '../helpers/Validators';
 
 const initialForm = {    
     email:'',
     password:'',
 }
 
-const validarCorreo = (email = '') => {
-    const regexCorreo = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
-    let mensajeError = null;
-    if(!email.trim()) {
-        mensajeError = 'El campo email es requerido';
-    } else if (!regexCorreo.test(email)) {
-        mensajeError = 'Formato de email incorrecto';
-    }
-    return mensajeError;
-}
+const validador = new Validator();
 
 const validarFormulario = (formulario) => {
     const errores = {};
-    if(validarCorreo(formulario.email)) {
-        errores.email = validarCorreo(formulario.email);
+    if(validador.validarCorreo(formulario.email)) {
+        errores.email = validador.validarCorreo(formulario.email);
+    }
+    if(validador.validarContrasenia(formulario.password)) {
+        errores.password = validador.validarContrasenia(formulario.password);
     }
     return errores;
 }
 
 const FormularioLogin = () => {
-    const {form, error, cargando, respuesta, handleChange, handleBlur, handleSubmit} = useForm(initialForm, validarFormulario, 'login');
+    const {form, error, loading, response, statusCode, handleChange, handleBlur, handleSubmit} = useForm(initialForm, validarFormulario, 'login');    
     return (
         <div className="form-container">
             <header className="header-login">
                 <h2>Iniciar sesión</h2>
-            </header>
+            </header>        
             <form onSubmit={handleSubmit} className="form-login">
-                <label htmlFor="email">Correo electronico</label>
-                <input type="email" name="email" className = "input" placeholder="Escribe tu correo electronico" onBlur={handleBlur} onChange={handleChange} value={form.email} required/>          
-                <label htmlFor="password">Contraseña</label>
-                <input type="password" name="password" className="input" placeholder="Escribe tu contraseña" onBlur={handleBlur} onChange={handleChange} value={form.password} required/>                            
-                <div className="button-container">
-                    <button type="submit" value="IniciarSesion" className="button-signup">Iniciar sesión</button>
-                    <p>¿No tienes una cuenta? <Link to="/signup" className="link-signup">Registrate</Link></p>
-                </div>
+                <p className="row">
+                    <label htmlFor="email">Correo electronico</label>
+                    <input type="email" name="email" className = "input" placeholder="Escribe tu correo electronico" pattern={validador.regexCorreo.toString().replaceAll('/', '')} onBlur={handleBlur} onChange={handleChange} value={form.email} required/>          
+                    {error.email && <em><small>{error.email}</small></em>}
+                </p>
+                <p className="row">
+                    <label htmlFor="password">Contraseña</label>
+                    <input type="password" name="password" className="input" placeholder="Escribe tu contraseña" pattern={validador.regexContrasenia.toString().replaceAll('/', '')} onBlur={handleBlur} onChange={handleChange} value={form.password} required/>
+                    {error.password && <em><small>{error.password}</small></em>}
+                </p>
+                <p className="row center">
+                    <button type="submit" value="IniciarSesion" className="button-signup">Iniciar sesión</button>                    
+                </p>
+                <p className="row center">¿No tienes una cuenta? <Link to="/signup" className="link-signup">Registrate</Link></p>
             </form>
-            {cargando && <Loader />}
-            {respuesta && <Message msg="Enviado" backgroundColor = "#198754"/>}
+            {loading && <Loader />}
+            {response && <Message msg={codeMessage[statusCode]} backgroundColor = "#198754"/>}
             {!error}
         </div>
     )

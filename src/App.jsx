6550c27e75d281352084cfa45619f19c99465a59
Alexis Alvarez <alexisao@hotmail.com>
2021-10-19 17:45:28 -0500

    Se agregó la ruta del Refugioy la opcion en el menú lateral

--- src/App.jsx
+++ src/App.jsx
@@ -8,16 +8,18 @@ import MenuLateral from './components/MenuLateral';
 import Login from './pages/Login';
 import Registro from './pages/Registro';
 import Inicio from './pages/Inicio';
+import Refugio from './pages/Refugio';
 
-function App() {
+function App() {  
   return (
-    <div className="App">
+    <div className="App">      
       <Router>
         <MenuCabecera />
         <MenuLateral />        
         <Switch>
           <Route exact path="/login"><Login layoutPosition="main"/></Route>
           <Route exact path="/signup"><Registro layoutPosition="main"></Registro></Route>
+          <Route exact path="/refugio"><Refugio layoutPosition="main"></Refugio></Route>
           <Route exact path="/" component={Inicio}/>
         </Switch>
       </Router>
--- src/components/MenuLateral.jsx
+++ src/components/MenuLateral.jsx
@@ -7,7 +7,7 @@ const MenuLateral = () => {
         <aside className="panel aside-1">
             <nav className="nav-menu">
                 <Link to="/">Perfil</Link>
-                <Link to="/">Opcion 2</Link>
+                <Link to="/refugio">Refugios</Link>
                 <Link to="/">Opcion 3</Link>
                 <Link to="/">Opcion 4</Link>
                 <Link to="/">Opcion 5</Link>
